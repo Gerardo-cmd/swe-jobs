6bf8f00296396c35a003e7947a27711aed4b5a6f
Gerardo Abaunza <56178845+Gerardo-cmd@users.noreply.github.com>
2021-12-09 10:48:44 -0500

    Validate form inputs

--- jobs/src/components/pages/NewPosition.tsx
+++ jobs/src/components/pages/NewPosition.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Input from '@mui/material/Input';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import TextField from '@mui/material/TextField';
@@ -8,7 +7,7 @@ import InputAdornment from '@mui/material/InputAdornment';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Header from '../sub-components/header';
-import { Navigate, useNavigate } from 'react-router';
+import { useNavigate } from 'react-router';
 
 
 const environments = [
@@ -43,9 +42,9 @@ const NewPosition: React.FC = () => {
     const handleSubmit = (e: any) => {
         e.preventDefault();
         resetErrors();
-        const positionTitle = e.target[0].value;
-        const company = e.target[1].value;
-        const salary = e.target[2].value;
+        const positionTitle = e.target.positionTitle.value;
+        const company = e.target.company.value;
+        const salary = e.target.salary.value;
         let error = false;
         if (positionTitle.trim() === "") {
             setPositionTitleError(true);
@@ -69,58 +68,58 @@ const NewPosition: React.FC = () => {
     return (
         <div>
             <Header title={"New Position"}/>
-            <form onSubmit = {handleSubmit}>
-                <Box
-                    component="form"
-                    sx={{
-                        '& .MuiTextField-root': { m: 1, width: '25ch' },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                >
-                    <div>
-                        <TextField id="outlined-basic" error={positionTitleError} label="Position Title" variant="outlined" />
-                    </div>
-                    <div>
-                        <TextField id="outlined-basic" error={companyError} label="Company" variant="outlined" />
-                    </div>
-                    <div>
-                        <FormControl sx={{ m: 1 }}>
-                            <InputLabel htmlFor="outlined-adornment-amount">Yearly Salary</InputLabel>
-                            <OutlinedInput
-                                id="outlined-adornment-amount"
-                                error={salaryError}
-                                startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                                label="Yearly Salary"
-                            />
-                        </FormControl>
-                    </div>
-                    <div>
-                        <TextField
-                            id="standard-select-currency-native"
-                            select
-                            label="Environment"
-                            value={environment}
-                            onChange={handleChange}
-                            SelectProps={{
-                                native: true,
-                            }}
-                            helperText="Select One"
-                            variant="standard"
-                            >
-                            {environments.map((option) => (
-                                <option key={option.value} value={option.value}>
-                                    {option.value}
-                                </option>
-                            ))}
-                        </TextField>
-                    </div>
-                    <div>
-                        {/* Try using Material-UI's Input that's already imported at the top? */}
-                        <input type="submit" value="Submit" />
-                    </div>
-                </Box>
-            </form>
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+                onSubmit={handleSubmit}
+            >
+                <div>
+                    <TextField id="outlined-basic" error={positionTitleError} label="Position Title" variant="outlined" name="positionTitle" />
+                </div>
+                <div>
+                    <TextField id="outlined-basic" error={companyError} label="Company" variant="outlined" name="company" />
+                </div>
+                <div>
+                    <FormControl sx={{ m: 1 }}>
+                        <InputLabel htmlFor="outlined-adornment-amount">Yearly Salary</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-amount"
+                            error={salaryError}
+                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                            label="Yearly Salary"
+                            name="salary"
+                        />
+                    </FormControl>
+                </div>
+                <div>
+                    <TextField
+                        id="standard-select-currency-native"
+                        select
+                        label="Environment"
+                        value={environment}
+                        onChange={handleChange}
+                        SelectProps={{
+                            native: true,
+                        }}
+                        helperText="Select One"
+                        variant="standard"
+                        >
+                        {environments.map((option) => (
+                            <option key={option.value} value={option.value}>
+                                {option.value}
+                            </option>
+                        ))}
+                    </TextField>
+                </div>
+                <div>
+                    {/* Try using Material-UI's Input that's already imported at the top? */}
+                    <Input type="submit" value="Submit" />
+                </div>
+            </Box>
         </div>
     );
 }
